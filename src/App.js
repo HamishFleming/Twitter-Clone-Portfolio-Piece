61eefcd780d58d6c6e6c2a08279686c6de1d8efa
Hamish Fleming <fleming_hamish@yahoo.com>
2021-04-20 23:45:36 +1000

    more formatting, added menu bar

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import "./App.css";
 import Feed from "./Feed";
 import Sidebar from "./Sidebar";
 import Widgets from "./Widgets";
+import MobileNav from "./MobileBottom";
 import MediaQuery from 'react-responsive';
 
 function App() {
   return (
     <div className="app">
+      <MediaQuery minWidth={401}>
       <Sidebar />
+      </MediaQuery>
       <Feed />
       <MediaQuery minWidth={1000}>
       <Widgets />
+      </MediaQuery>
+      <MediaQuery maxWidth={400}>
+      <MobileNav />
       </MediaQuery>
     </div>
   );
